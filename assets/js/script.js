89255d24a21483102fd7633fa002853228eacfd7
Ulemjin Badral <ulemjin@gmail.com>
2021-03-11 11:12:26 -0500

    save tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,7 @@ var createTaskEl = function(taskDataObj) {
     taskDataObj.id = taskIdCounter;
 
     tasks.push(taskDataObj);
+    saveTasks();
 
     // increase task counter for next unique id
     taskIdCounter++;
@@ -85,6 +86,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
         }
     };
     alert("Task updated!");
+
+    saveTasks();
 
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
@@ -167,6 +170,8 @@ var deleteTask = function(taskId) {
 
     // reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+
+    saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -210,7 +215,13 @@ var taskStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
+
+    saveTasks();
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 formEl.addEventListener("submit", taskFormHandler);
 
